f319e971bbba3b2d3018a78bd63930f789df8f79
Oleg <igrok_be@hotmail.com>
2020-11-20 13:02:57 +0100

    moved to Xlib for ui (#3)
    * added initiwindowposition and initwindowsize functions
    fixed passing of args to glutinit function
    
    * moved to more low level api for windowing system

---
+++ src/org/igrok_net/engine/ui/IGNWindow.java
@@ -0,0 +1,42 @@
+package org.igrok_net.engine.ui;
+
+import java.io.File;
+
+public class IGNWindow {
+    static {
+        File nativeLib = new File("lib/libIGNUI.so");
+        System.load(nativeLib.getAbsolutePath());
+    }
+
+    /**
+     * Native window pointer
+     */
+    private long wndPtr;
+
+    private native long CreateNativeWindow(String title, int x, int y, int width, int height);
+
+    private native void DestroyWindow(long wndPtr);
+
+    public IGNWindow() {
+        this("Igrok-NET Engine");
+    }
+
+    public IGNWindow(String title) {
+        this(title, 0, 0);
+    }
+
+    public IGNWindow(String title,int x,int y) {
+        this(title, x, y, 800, 600);
+    }
+
+    public IGNWindow(String title, int x, int y, int width,int height) {
+        wndPtr = CreateNativeWindow(title, x, y, width,height);
+    }
+
+    public void Dispose(){
+        if(this.wndPtr > 0){
+            DestroyWindow(this.wndPtr);
+        }
+        this.wndPtr = 0;
+    }
+}
