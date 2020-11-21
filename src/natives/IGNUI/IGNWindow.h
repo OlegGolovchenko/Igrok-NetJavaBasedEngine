cf5b1c81ffb2f9b033af40bef4908f7d68a32d1d
Oleg <igrok_be@hotmail.com>
2020-11-21 22:36:08 +0100

    Added mouse and key events (#6)
    * added initiwindowposition and initwindowsize functions
    fixed passing of args to glutinit function
    
    * moved to more low level api for windowing system
    
    * updated readme file
    
    * added fully functional xlib glx window
    no context is yet initialized
    
    * Added fully functional key events
    Added button press events for classic mouse (3button)

--- src/natives/IGNUI/IGNWindow.h
+++ src/natives/IGNUI/IGNWindow.h
@@ -7,18 +7,18 @@
 class IGNWindow
 {
 private:
-    Display *display;
-    Window window;
     Window root;
     XVisualInfo *vi;
     GLint att[5] = {GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None};
     Colormap cmap;
     XSetWindowAttributes *swa;
     GLXContext glc;
-    bool running;
 
 public:
+    Display *display;
+    Window window;
+    bool running;
     IGNWindow(const char *title, int x, int y, int width, int height);
-    void MainLoop();
+    bool IsRunning();
     ~IGNWindow();
 };
