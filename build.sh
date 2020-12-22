00261c381b97360629b9b12c54bafe905536afe2
Oleg Golovchenko <igrok_be@hotmail.com>
2020-12-22 15:23:46 +0100

    added jar to build process

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 echo "compiling java files and generating native headers"
 rm -r ./bin/*
 javac -d ./bin -cp ./bin -h ./src/natives ./src/org/igrok_net/engine/ui/events/*.java ./src/org/igrok_net/engine/ui/*.java ./src/org/igrok_net/launcher/*.java
+chmod -R 775 bin
+rm -r ./pkg/*
+cd ./pkg
+jar cvf org.igrok-net.engine.jar -C ../bin org/igrok_net/engine/
+jar cvfe org.igrok-net.launcher.jar org.igrok_net.launcher.Main -C ../bin org/igrok_net/launcher
+cd ..
 echo "done."
 echo "compiling native library"
 rm ./obj/IGNUI.o
