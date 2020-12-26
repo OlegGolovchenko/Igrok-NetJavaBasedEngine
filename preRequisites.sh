d392c70f8459a3e64ab040ee6cc27c1d40d1f204
Oleg Golovchenko <igrok_be@hotmail.com>
2020-12-26 19:57:40 +0100

    fully fleshed out prerequisites.sh file updated readme with latest files

--- preRequisites.sh
+++ preRequisites.sh
@@ -5,4 +5,8 @@ apt-get upgrade -y
 apt-get update
 apt-get dist-upgrade -y
 apt-get update
-apt-get install freeglut3 freeglut3-dev libglew-dev mesa-utils libx11-dev xorg -y
+apt-get install lsb-release git gcc wget apt-transport-https gnupg freeglut3 freeglut3-dev libglew-dev mesa-utils libx11-dev xorg -y
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb "$1" main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+sudo apt-get update
+sudo apt-get install adoptopenjdk-15-hotspot adoptopenjdk-15-hotspot-jre -y
